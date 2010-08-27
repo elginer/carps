dae153191c9c97613887a90fdd99ac2ef30b20c6
Johnny Mo <spoon@killersmurf.com>
2010-08-27 12:57:19 +0100

    Altered change.sh to use a modal system: edit mode for working on program, change mode for commits

--- change.sh
+++ change.sh
@@ -3,8 +3,6 @@
 # Small script to run whenever I make a change
 
 # Move sensitive data out of the folder, commit and move it back
-mv -n email.yaml server_email.yaml .. && \
-mv -n .key ../carps_key && \
+./commit_mode.sh && \
 git add -A && git commit && \
-mv -n ../email.yaml ../server_email.yaml && \
-mv -n ../carps_key .
+./edit_mode.sh
---
+++ commit_mode.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+mv -n email.yaml server_email.yaml .. && \
+mv -n .key ../carps_key
